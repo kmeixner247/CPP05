9e58de7bd4c75143304193bfd33b38dd53128a47
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-07-04 21:02:04 +0200

    more tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 22:14:56 by kmeixner          #+#    #+#             */
-/*   Updated: 2022/07/04 20:35:51 by kmeixner         ###   ########.fr       */
+/*   Updated: 2022/07/04 21:01:27 by kmeixner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ int main()
 {
 	Intern unnamedIntern;
 	Bureaucrat bill("Bill", 1);
-	AForm *someform = unnamedIntern.makeForm("shrubery creation", "42");
-	if (someform)
-	{
-		bill.signForm(*someform);
-		bill.executeForm(*someform);
-		std::cout << *someform << std::endl;
-		delete someform;
-	}
+	AForm *form1 = unnamedIntern.makeForm("shrubbery creation", "42");
+	AForm *form2 = unnamedIntern.makeForm("robotomy request", "your mom");
+	AForm *form3 = unnamedIntern.makeForm("presidential pardon", "your dad");
+	AForm *form4 = unnamedIntern.makeForm("world domination", "the world");
+	std::cout << "Form 1: " << *form1 << std::endl;
+	std::cout << "Form 2: " << *form2 << std::endl;
+	std::cout << "Form 3: " << *form3 << std::endl;
+	if (!form4)
+		std::cout << "Form 4 doesn't exist!" << std::endl;
+	delete form1;
+	delete form2;
+	delete form3;
 }
